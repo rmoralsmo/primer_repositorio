1819573b75ff8f3690f52ca93a3e301888cabb92
rmorales <rmorales@it-group.com.mx>
2017-03-24 13:33:57 -0600

    commit

--- Helloworld/itgroup/com/Helloworld.java
+++ Helloworld/itgroup/com/Helloworld.java
@@ -4,7 +4,7 @@ public class Helloworld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola mundo");
+		System.out.println("Hola mund");
 
 	}
 
