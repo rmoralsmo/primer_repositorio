685b37b5ef7798479999787854fac6daf8731c15
rmoralesmo <rmorales@it-group.com.mx>
2017-03-27 11:30:32 -0600

    se agrego otro system

--- Helloworld/itgroup/com/Helloworld.java
+++ Helloworld/itgroup/com/Helloworld.java
@@ -5,6 +5,7 @@ public class Helloworld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo, mundo rmorals");
+		System.out.println("estamos probando");
 
 	}
 
